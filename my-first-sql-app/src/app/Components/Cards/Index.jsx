462adb94060cbf42b62b43e7c3ed67e3a0bdab12
Otsile Katane <otsilekatane@gmail.com>
2024-02-13 15:14:22 +0200

    adding pictures

--- my-first-sql-app/src/app/Components/Cards/Index.jsx
+++ my-first-sql-app/src/app/Components/Cards/Index.jsx
@@ -2,11 +2,11 @@ export default function AllCards() {
     return(
         <div>
             <h1>My Cards</h1>
-            <img src="my-first-sql-app/Cards/Clicks.jpeg" alt="Clicks" width="50px" height="50px" />
-            <img src="my-first-sql-app/Cards/DIschem.webp" alt="DIschem" width="50px" height="50px" />
-            <img src="my-first-sql-app/Cards/Pick n pay.jpg" alt="Pick n pay" width="50px" height="50px" />
-            <img src="my-first-sql-app/Cards/Shoprite.png" alt="Shoprite" width="50px" height="50px" />
-            <img src="my-first-sql-app/Cards/Woolworths.png" alt="Woolworths" width="50px" height="50px" />
+            <img src="my-first-sql-app\Cards\Clicks.jpeg" alt="Picture Unavailable" width="50px" height="50px" />
+            <img src="my-first-sql-app\Cards\DIschem.webp" alt="Picture Unavailable" width="50px" height="50px" />
+            <img src="my-first-sql-app\Cards\Pick n pay.jpg" alt="Picture Unavailable" width="50px" height="50px" />
+            <img src="my-first-sql-app\Cards\Shoprite.png" alt="Picture Unavailable" width="50px" height="50px" />
+            <img src="my-first-sql-app\Cards\Woolworths.png" alt="Picture Unavailable" width="50px" height="50px" />
 
         </div>
     )
