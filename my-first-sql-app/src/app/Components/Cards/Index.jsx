83121da8a8c430cce249aef8f6aabc3049b4be91
Otsile Katane <otsilekatane@gmail.com>
2024-02-13 16:54:17 +0200

    more images

--- my-first-sql-app/src/app/Components/Cards/Index.jsx
+++ my-first-sql-app/src/app/Components/Cards/Index.jsx
@@ -2,7 +2,7 @@ export default function AllCards() {
     return(
         <div>
             <h1>My Cards</h1>
-            <img src="my-first-sql-app\Cards\Clicks.jpeg" alt="Picture Unavailable" width="50px" height="50px" />
+            <img src="https://www.flaticon.com/free-icon/shopping-cart_2838895?term=shopping&page=1&position=3&origin=search&related_id=2838895" alt="Picture Unavailable" width="50px" height="50px" />
             <img src="my-first-sql-app\Cards\DIschem.webp" alt="Picture Unavailable" width="50px" height="50px" />
             <img src="my-first-sql-app\Cards\Pick n pay.jpg" alt="Picture Unavailable" width="50px" height="50px" />
             <img src="my-first-sql-app\Cards\Shoprite.png" alt="Picture Unavailable" width="50px" height="50px" />
