d596f4b6a9c76babd6ba097c34d1cc6802ef7b62
Otsile Katane <otsilekatane@gmail.com>
2024-02-15 14:51:59 +0200

    additional functionality

--- my-first-sql-app/src/app/page.js
+++ my-first-sql-app/src/app/page.js
@@ -1,22 +1,24 @@
+// firstPage.jsx
+import React from 'react';
+import useRouter  from 'next/router';
 import Header from "./Components/header/Index";
 import AllCards from "./Components/Cards/Index";
+import { info } from "./Components/help/Index";
+
 export default function firstPage() {
-  return (
-    <div>
-      <Header/>
-      <div className="buttons">
-      <button>Help</button>
-      <button>My Cards</button>
-      <button>My Points</button>
-      <button>Settings</button>
-      <button>About</button>
-      </div>
-      <h1>
-        Welcome!!!
-      </h1>
-      <p>Welcome to my welcome page. This is the first of many posts that will get me to undderstand all this better</p>
-      <AllCards/>
-    </div>
-  );
-    
+    return (
+        <div>
+            <Header />
+            <div className="buttons">
+                <button onClick={info}>Help</button>
+                <button>My Cards</button>
+                <button>My Points</button>
+                <button>Settings</button>
+                <button>About</button>
+            </div>
+            <h1>Welcome!!!</h1>
+            <p>Welcome to my welcome page. This is the first of many posts that will get me to understand all this better</p>
+            <AllCards />
+        </div>
+    );
 }
