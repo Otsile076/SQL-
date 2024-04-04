674e1c7106a3c9d7f4c7417dd6848e78709fc98d
Otsile Katane <otsilekatane@gmail.com>
2024-04-04 20:15:42 +0200

    Body addition

---
+++ my-first-sql-app/src/app/Components/Body/Index.jsx
@@ -0,0 +1,9 @@
+export default function Bod() {
+    return(
+        <div className="head">
+            <h1>
+                This is where all the information about the site will go once we've figures out what we want on the body
+            </h1>
+        </div>
+    )
+};
